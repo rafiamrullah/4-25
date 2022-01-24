534ae578f6ba1a2262f920e70a929a331438e2a9
rafiamrullah <rafiamrullah2112@gmail.com>
2022-01-24 20:22:25 +0700

    Uploud Modul

---
+++ routes/web.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('Home', [
+        "title" => "Home"
+    ]);
+});
+
+Route::get('/about', function () {
+    return view('About', [
+        "title" => "About",
+        "nama" => "Koro-Sensei",
+        "email" => "rafiamrullah2112@gmail.com",
+        "gambar" => "koro sensei.jpg"
+    ]);
+});
+
+Route::get('/gallery', function () {
+    return view('Gallery', [
+        "title" => "Gallery"
+    ]);
+});
